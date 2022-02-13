909f9a27bb902162188e195d9b696e78fc10232b
Murtaza Khan <murtaza.khan1995@gmail.com>
2022-02-13 22:03:56 +0500

    added connection with yarn

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		createConnection({ type: "sqlite", database: "db.sqlite3", entities: [] })
+			.then(connection => {
+				console.log("CONNECTION SUCCESSFUL");
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
